7ce9cd347db8ac4a83f479dffbac40a9c24fb804
yky <generic.intelligence@gmail.com>
2018-05-22 00:09:54 +0800

    cantonese, minor

---
+++ cantonese/find-unique-pinyins.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Notice that by pinyin I mean spellings for Cantonese.
+
+# Input: list with char + pinyin
+# Output: list of unique pinyins (different tones can be regarded as equal or unequal)
+
+import sys
+
+f1 = open("yale-raw.txt", "r")			# format: char,pinyin with tone number
+# fo = sys.stdout
+fo = open("yale-unique-pinyins.txt", "w")
+
+big_sum = 0
+freq_sum = 0
+last_pinyin = "a"							# note: this must be the first pinyin in list
+
+pinyins = []
+
+for line in f1:
+	pinyins.append(line.split(' '))
+
+for line in f2:
+	items = line.split(',')
+	# print line
+
+	char = items[0]
+	# freq = float(items[1][:-1])
+
+	found = False
+	for item in pinyins:
+		if char == item[0]:
+			fo.write(char + ',' + item[1])
+			found = True
+
+	if not found:
+		print char, " not found!!"
+
+	if False: """
+	# remove tone number at the end
+	if pinyin_tone[-1].isdigit():
+		# tone = '0'
+		pinyin = pinyin_tone[:-1]
+	else:
+		pinyin = pinyin_tone
+	"""
+
+	# fo.write('[\'' + k + '\',\'' + n + '\',%.9f],\n' % freq_sum)
+
+#==========================================================================================================
+
+# Input: list sorted by pinyins
+# Output: frequencies consolidated for each unique pinyin
+# for this version we may ignore tone numbers
+
+import sys
+
+f = open("char-pinyin2-freq_by-pinyin.csv", "r")
+# fo = sys.stdout
+fo = open("pinyins-consolidated.csv", "w")
+
+# Input Format:
+# pinyin-with-tone, char, frequency \n
+
+# Output Format:
+# consonant, nucleus, cumulative frequency \n
+# old version: pinyin-without-tone, cumulative frequency \n
+
+consonants = [ 'b','p','m','f','d','t','n','l','g','k','h','j','q','w','x','y','r' ]
+# 'zh','ch','sh','z','c','s' are treated as special cases
+
+# ***** convert to consonant-nucleus form *****
+def k_n(pinyin_):
+	# test for special cases:  z,c,s,zh,ch,sh
+	c = pinyin_[0]
+	consonant = None
+	if (c == 'z' or c == 'c' or c == 's'):
+		if (pinyin_[1] == 'h'):
+			consonant = c + 'h'
+			nucleus = pinyin_[2:]
+		else:
+			consonant = c
+			nucleus = pinyin_[1:]
+	else:						# test for the rest
+		for k in consonants:
+			if (pinyin_[0] == k):
+				consonant = c
+				nucleus = pinyin_[1:]
+	if (consonant == None):
+		consonant = ''
+		nucleus = pinyin_
+	return [consonant, nucleus]
+
+big_sum = 0
+freq_sum = 0
+last_pinyin = "a"							# note: this must be the first pinyin in list
+
+for line in f:
+	items = line.split(',')
+	print items
+
+	pinyin_tone = items[0]				# pinyin with tone at the end
+	char = items[1]
+	freq = float(items[2][:-1])
+
+	# remove tone number at the end
+	if pinyin_tone[-1].isdigit():
+		# tone = '0'
+		pinyin = pinyin_tone[:-1]
+	else:
+		pinyin = pinyin_tone
+
+	############ consolidate frequencies ##############
+	if pinyin == last_pinyin:
+		freq_sum += freq
+	else:
+		[k, n] = k_n(last_pinyin)
+		fo.write('[\'' + k + '\',\'' + n + '\',%.9f],\n' % freq_sum)
+		print '[\'' + k + '\',\'' + n + '\',%.9f],\n' % freq_sum
+		big_sum += freq_sum
+		last_pinyin = pinyin
+		freq_sum = freq
+
+# got to take care of the last bit -- annoying!
+[k, n] = k_n(last_pinyin)
+fo.write('[\'' + k + '\',\'' + n + '\',%.9f]\n' % freq_sum)
+print '[\'' + k + '\',\'' + n + '\',%.9f]\n' % freq_sum
+
+# OK, print the grand total and exit...
+print big_sum
